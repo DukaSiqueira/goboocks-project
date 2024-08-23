e168937cf0bb5f8653fb68caffbbdbd962218fc8
DukaSiqueira <e.siqueira@edu.unipar.br>
2024-08-22 22:09:17 -0300

    First Commit

---
+++ internal/service/book.go
@@ -0,0 +1,64 @@
+package service
+
+import "database/sql"
+
+type Book struct {
+	ID     int
+	Title  string
+	Author string
+	Genre  string
+}
+
+type BookService struct {
+	db *sql.DB
+}
+
+func (s *BookService) CreateBook(book Book) error {
+	query := "INSERT INTO books (title, author, genre) VALUES (?, ?, ?)"
+	result, err := s.db.Exec(query, book.Title, book.Author, book.Genre)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	book.ID = int(id)
+	return nil
+}
+
+func (s *BookService) GetBook() ([]Book, error) {
+	query := "SELECT id, title, author, genre FROM books"
+	rows, err := s.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	var books []Book
+	for rows.Next() {
+		var book Book
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Genre)
+		if err != nil {
+			return nil, err
+		}
+
+		books = append(books, book)
+	}
+
+	return books, nil
+}
+
+func (s *BookService) GetBookByID(id int) (*Book, error) {
+	query := "SELECT id, title, author, genre FROM books WHERE id = ?"
+	row := s.db.QueryRow(query, id)
+
+	var book Book
+	err := row.Scan(&book.ID, &book.Title, &book.Author, &book.Genre)
+	if err != nil {
+		return nil, err
+	}
+
+	return &book, nil
+}
